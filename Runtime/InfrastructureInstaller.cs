fbad959aed7520e7817667a7ab1f7fd4317252d7
unknown <rasoul.shahhoseini@gmail.com>
2023-01-24 19:06:56 +0330

    :sparkle: initial commit

---
+++ Runtime/InfrastructureInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Northgard.Core.Abstraction.Logger;
+using Northgard.Infrastructure.Logger;
+using Zenject;
+
+namespace Northgard.Infrastructure
+{
+    public class InfrastructureInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ILogger>().To<UnityLogger>().FromNew().AsSingle();
+        }
+    }
+}
---
+++ Runtime/Logger/UnityLogger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using ILogger = Northgard.Core.Abstraction.Logger.ILogger;
+
+namespace Northgard.Infrastructure.Logger
+{
+    internal class UnityLogger : ILogger
+    {
+        public void LogMessage(string msg)
+        {
+            Debug.Log(msg);
+        }
+
+        public void LogWarning(string msg)
+        {
+            Debug.LogWarning(msg);
+        }
+
+        public void LogError(string msg)
+        {
+            Debug.LogError(msg);
+        }
+    }
+}
